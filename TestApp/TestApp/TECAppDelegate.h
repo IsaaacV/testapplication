46fad6ddcc779a4adcf12e3d0001cbf2987dc7cd
Isaac Verniers <isaacverniers@reference.be>
2014-03-03 22:04:21 +0100

    Create

---
+++ TestApp/TestApp/TECAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TECAppDelegate.h
+//  TestApp
+//
+//  Created by Isaac Verniers on 3/03/14.
+//  Copyright (c) 2014 Test Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TECAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Isaac Verniers on 3/03/14.
+//  Copyright (c) 2014 Test Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TECAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TECAppDelegate class]));
+    }
+}
