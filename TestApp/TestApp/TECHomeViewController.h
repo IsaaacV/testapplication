2b6446277f379b154817e1930506dc543704db62
Isaac Verniers <isaacverniers@reference.be>
2014-03-03 22:53:39 +0100

    Add new home view controllers.

---
+++ TestApp/TestApp/TECHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TECHomeViewController.h
+//  TestApp
+//
+//  Created by Isaac Verniers on 3/03/14.
+//  Copyright (c) 2014 Test Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TECHomeViewController : UIViewController
+
+@end
---
+++ TestApp/TestApp/TECHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  TECHomeViewController.m
+//  TestApp
+//
+//  Created by Isaac Verniers on 3/03/14.
+//  Copyright (c) 2014 Test Company. All rights reserved.
+//
+
+#import "TECHomeViewController.h"
+
+@interface TECHomeViewController ()
+
+@end
+
+@implementation TECHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
